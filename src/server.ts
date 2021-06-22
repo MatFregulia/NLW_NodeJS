7c08920d05d84162b8bf522abfa6b50bbfebf3de
MatFregulia <mat.fregulia@gmail.com>
2021-06-21 23:12:51 -0300

    Day 2 - Criando estutura de usuário - BD + migrations + entidade + repositório + service + controller - Party_1

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import "reflect-metadata";
+
+import "./database";
+
+const app = express();
+
+//http://localhost:3000
+app.listen(3000, () => 
+    console.log("Server is running"));
